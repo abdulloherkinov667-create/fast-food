01d0e3ac7f40c1ab432e20b9c3f2c400efa5b885
abdulloherkinov667-create <abdulloherkinov667@gmail.com>
2026-06-27 00:40:17 +0500

    model yozildi product uchun

--- products/models.py
+++ products/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class BaseCreateModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        
+
+class Category(BaseCreateModel):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+
+class FastFoodProduct(BaseCreateModel):
+    name = models.CharField(max_length=500)
+    price = models.DecimalField("Maxsulot narxi: ", max_digits=10, decimal_places=2)
+    ingredients = models.TextField("Tarkibi: ", max_length=4000)
+    category_product = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="food_products")
+    product_image = models.ImageField(upload_to="products/images/")
+    is_active = models.BooleanField(default=True)
+    
+    class Meta:
+        verbose_name = "Mahsulot"
+        verbose_name_plural = "Mahsulotlar"
