464124d337c8e64797f0337c15fc49981052ebdc
abdulloherkinov667-create <abdulloherkinov667@gmail.com>
2026-06-27 22:39:47 +0500

    ..nimadur qildim de...

--- root/urls.py
+++ root/urls.py
@@ -18,11 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from products import views
 from products.views import menu_home
-from users.views import profile_page
+from users.views import profile_page, savat_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', menu_home, name='menu_home'),
-    path("profil/", profile_page, name='profile_user')
+    path("profil/", profile_page, name='profile_user'),
+    path("savat_page/", savat_page, name='savat_page')
 ]
     
--- users/views.py
+++ users/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def profile_page(request):
     return render(request, "user_page/profil_page.html")
+
+
+def savat_page(request):
+    return render(request, "user_page/savat.html")
